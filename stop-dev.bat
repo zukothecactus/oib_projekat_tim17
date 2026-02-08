cf743e8d3b5bf2de3849ae08a1eb4665e48304a2
David Stevanovic <dstevanovic238@gmail.com>
2026-02-08 23:33:17 +0100

    Minor fix

--- stop-dev.bat
+++ stop-dev.bat
@@ -3,7 +3,7 @@ echo ==============================
 echo Stopping ALL services on ports
 echo ==============================
 
-set PORTS=5000 5001 5002 5003 5004 5006 5007 5100 5173
+set PORTS=5000 5001 5002 5003 5004 5005 5006 5007 5008 5100 5173
 
 for %%P in (%PORTS%) do (
     echo.
