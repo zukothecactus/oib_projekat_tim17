886f4f6aa655b4688ef4cd2b20a46811e2404f40
Andrija <andrijanstanisic321@gmail.com>
2026-02-08 15:16:48 +0100

    feat: Add databases and fix microservice problems

--- create_databases.sql
+++ create_databases.sql
@@ -10,5 +10,23 @@ CREATE DATABASE IF NOT EXISTS `production_db` CHARACTER SET utf8mb4 COLLATE utf8
 -- Kreiranje baze za Processing mikroservis
 CREATE DATABASE IF NOT EXISTS `processing_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- Kreiranje baze za Audit mikroservis (evidencija dogadjaja)
+CREATE DATABASE IF NOT EXISTS `audit_logovi` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Kreiranje baze za Packaging mikroservis (pakovanje)
+CREATE DATABASE IF NOT EXISTS `packaging_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Kreiranje baze za Storage mikroservis (skladistenje)
+CREATE DATABASE IF NOT EXISTS `storage_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Kreiranje baze za Sales mikroservis (prodaja)
+CREATE DATABASE IF NOT EXISTS `sales_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Kreiranje baze za Analytics mikroservis (analiza podataka)
+CREATE DATABASE IF NOT EXISTS `izvestaji_analize` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Kreiranje baze za Performance mikroservis (analiza performansi)
+CREATE DATABASE IF NOT EXISTS `izvestaji_performanse` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- Prikaz kreiranih baza
-SHOW DATABASES LIKE '%_db';
+SHOW DATABASES;
